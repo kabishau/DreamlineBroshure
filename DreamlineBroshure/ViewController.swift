d24dc2d12ef5302ee525e1b790befeb85abc1926
Aleksey Kabishau <aliaksei.kabishau@gmail.com>
2018-08-03 23:55:49 -0400

    Initial Commit

---
+++ DreamlineBroshure/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DreamlineBroshure
+//
+//  Created by Aleksey Kabishau on 0803..18.
+//  Copyright © 2018 Aleksey Kabishau. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
